90b4d0ff84bcaff92a901dc3cc2b43ed8764d201
WilliamHannah-Tech <WilliamHannah.Tech@gmail.com>
2026-03-24 23:59:01 -0400

    Update test-bucket.tf

--- test-bucket.tf
+++ test-bucket.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
+    bucket  = "william-hannah-tfstate-2026"                 # Name of the S3 bucket
     key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
     region  = "us-east-2"                          # Use a variable for the region
     encrypt = true                                 # Enable server-side encryption (optional but recommended)
