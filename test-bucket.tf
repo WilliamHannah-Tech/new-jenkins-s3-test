7e02c4f88f8f5a64623788757984a6228a302e48
WilliamHannah-Tech <WilliamHannah.Tech@gmail.com>
2026-03-25 00:17:42 -0400

    Change S3 backend region from us-east-2 to us-east-1

--- test-bucket.tf
+++ test-bucket.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket  = "william-hannah-tfstate-2026"                 # Name of the S3 bucket
     key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-2"                          # Use a variable for the region
+    region  = "us-east-1"                          # Use a variable for the region
     encrypt = true                                 # Enable server-side encryption (optional but recommended)
   } 
 }
